45b89095a81988862ba7788d38e7d906b4b90136
Jacopo De maio <jajodemaio@gmail.com>
2024-08-03 17:18:22 +0200

    add parentesi

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -5,7 +5,7 @@ import tattooReducer from "../reducers/tattooReducer"
 import reservationReducer from "../reducers/reservationReducer"
 import { persistStore, persistReducer } from "redux-persist"
 import storage from "redux-persist/lib/storage"
-import encryptTransform from "redux-persist-transform-encrypt"
+import { encryptTransform } from "redux-persist-transform-encrypt"
 
 const rootReducer = combineReducers({
   user: fetchUserReducer,
