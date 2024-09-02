effc592893c79a2e167bcca9c2ae632039c5e437
Jacopo De maio <jajodemaio@gmail.com>
2024-09-02 18:14:22 +0200

    col size change

--- src/components/TattooArtistsPage.jsx
+++ src/components/TattooArtistsPage.jsx
@@ -55,7 +55,7 @@ const TattooArtistsPage = () => {
       <Row className="justify-content-center">
         {tattooArtistsArray?.length > 0 ? (
           tattooArtistsArray.map((tattooArtist, i) => (
-            <Col className="mb-3" xs={8} md={5} xl={4} xxl={3} key={i}>
+            <Col className="mb-3" xs={8} md={5} xl={4} xxl={4} key={i}>
               <TattooArtistCard
                 key={tattooArtist?.id}
                 tattooArtist={tattooArtist}
